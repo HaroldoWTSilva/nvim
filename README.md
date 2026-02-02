4163d0d2242cd722933c5199c4a2e5613c3f1613
Haroldo Watson <haroldo@rightside.ddns.net>
2026-02-02 12:25:18 -0300

    first

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# nvim
-Personal NeoVIM configuration files
+# My personal NeoVIM config repo
+
+Keeping my configs coherent among different development machines.
+
+## Install
+
+Install [tree-sitter-cli](https://github.com/tree-sitter/tree-sitter/releases) and [hack nerd font](https://www.nerdfonts.com/font-downloads).
+
+Then git clone this into the ~/.config/ folder, or into whatever your neovim config
+directory is.
+
+
---
+++ init.lua
@@ -0,0 +1,23 @@
+-- Interface
+vim.opt.number = true
+vim.opt.wrap = true
+vim.opt.mouse = "a"
+
+-- Indentação e tabs
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
+-- Limite de largura de linha
+vim.opt.textwidth = 80
+vim.opt.formatoptions:append("t")
+vim.opt.colorcolumn = "81"
+
+require("config.lazy")
+
+require'nvim-treesitter'.install { 
+'lua', 'javascript', 'php', 'awk', 'c', 'css', 'html', 'python', 'prolog' 
+}
+
+vim.cmd(":Neotree")
